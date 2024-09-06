6e5cecfd04479e8a7fe923c6393403ae1dd80d31
Jimmy García <jimmyegc@gmail.com>
2024-09-06 07:54:49 -0600

    fix 09062024

--- src/components/FormNested/FormNested.tsx
+++ src/components/FormNested/FormNested.tsx
@@ -52,12 +52,12 @@ function NestedFieldArray({
       </button>
       {fields.map((field, index) => (
         <fieldset key={field.id}        
-        draggable 
+        draggable         
         onDragStart={() => (dragAnswer.current = index)}
         onDragEnter={() => (draggedOverAnswer.current = index)}
         onDragEnd={() => move(dragAnswer.current, draggedOverAnswer.current)}
         onDragOver={(e)=> e.preventDefault()}
-        /* className={`${dragAnswer.current === index ? 'dragging' : ''}`} */
+        className={`${dragAnswer.current === index ? 'dragging' : ''}`} 
         style={{ border: '1px solid green'}}
         >
           <legend>Respuesta #{index+1}</legend>          
@@ -65,6 +65,29 @@ function NestedFieldArray({
             {...register(`fields.${parentFieldIndex}.answers.${index}.value`)}
             style={{ border: '1px solid gray', margin: 10}}
           />
+
+<fieldset>
+  <legend>Acciones:</legend>
+
+  <div>
+    <input type="radio" id="continue" name="action" value="continue" checked />
+    <label htmlFor="continue">Continuar</label>
+  </div>
+
+  <div>
+    <input type="radio" id="webform" name="action" value="webform" />
+    <label htmlFor="webform">Webform</label>
+  </div>
+  
+</fieldset>
+
+
+          <select name="" id="">
+            <option value="">Select</option>
+            <option value="">Webform #1</option>
+            <option value="">Webform #2</option>
+            <option value="">Webform #3</option>
+          </select>
           <button type="button" onClick={() => remove(index)}>
             Eliminar (<i>Respuesta</i>)
           </button>
@@ -122,6 +145,7 @@ export default function FormNested() {
         </button>
         {fields.map((field, index) => (
           <fieldset key={field.id} 
+            
             draggable 
             onDragStart={() => (dragQuestion.current = index)}
             onDragEnter={() => (draggedOverQuestion.current = index)}
@@ -142,8 +166,7 @@ export default function FormNested() {
               control={control}
               register={register}
               parentFieldIndex={index}
-            />
-            
+            />            
           </fieldset>
         ))}
         
